9c725f9aa20318fc7d9c88200afe43a028c9ca88
DennisSweeris <70529413+DennisSweeris@users.noreply.github.com>
2020-10-05 17:58:07 +0200

    Add files via upload

---
+++ M3_opdracht_01/index.js
@@ -0,0 +1,18 @@
+let name = "Dennis";
+console.log(name);
+
+const age = "29";
+console.log(age);
+
+let a = 2;
+let b = 4;
+let addition = (a + b);
+let substration = (a - b);
+let multiplication = (a * b);
+let division = (a / b);
+let modulus = (a % b);
+console.log(addition);
+console.log(substration);
+console.log(multiplication);
+console.log(division);
+console.log(modulus);
