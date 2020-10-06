b92412ed755b4bfb60207c5c7487ba1b27086330
DennisSweeris <70529413+DennisSweeris@users.noreply.github.com>
2020-10-06 12:39:46 +0200

    Add files via upload

---
+++ M3_opdracht_03/script.js
@@ -0,0 +1,23 @@
+const age = 18;
+
+if (age >= 18) {
+    console.log("Je mag naar binnen!");
+} else {
+    console.log("Je bent helaas te jong, blijft alsjeblieft buiten");
+}
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log("Je bent een vrouw!");
+} else {
+    console.log("Je bent iets anders!");
+}
+
+const driverStatus = "bob";
+
+if (driverStatus == "bob") {
+    console.log("Jij mag veilig rijden");
+} else {
+    console.log("Jij mag niet meer rijden");
+}
