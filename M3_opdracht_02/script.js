0256426afa78ae75f4219255f27cc7159da5938d
DennisSweeris <70529413+DennisSweeris@users.noreply.github.com>
2020-10-06 11:10:04 +0200

    Add files via upload

---
+++ M3_opdracht_02/script.js
@@ -0,0 +1,10 @@
+// Greeting
+console.log("Hallo Winc Academy studenten")
+
+/* Dit is een grote som
+let multiplication = 1230941 * 1823794;
+console.log("Multiplication:", multiplication);
+
+Divide
+let divide = 637263 / 54;
+console.log("Divide:", divide); */
